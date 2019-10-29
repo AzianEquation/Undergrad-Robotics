20acef9acb47b7325cd0a850958a030be3d4f298
John Esco <47394267+AzianEquation@users.noreply.github.com>
2019-10-29 12:05:05 -0400

    Add files via upload
    final project for robotics

--- README.txt
+++ README.txt
@@ -1,18 +1,40 @@
 
 README:
 
-Project 1 contains three python files: connection.py, interface.py, and project1.py
+Project 4 contains three python files: connection.py, interface.py,
+ and project4.py is the default interface given for establishing 
+serial connection between the computer and the raspberry pi. This interface is
+ able to open and close this serial communication as well as read and write
+ information from and to the robot. connection.py is not intended to be accessed 
+or used by the user. 
 
-connection.py is the default interface given for establishing serial connection between the computer and the raspberry pi. This interface is able to open and close this serial communication as well as read and write information from and to the robot. connection.py is not intended to be accessed or used by the user. 
-
-interface.py contains all the functions to initialize the robots state, read data, and move the robot. 
-start - starts the OI and set the robot to passive mode
+interface.py contains all the functions to initialize the
+ robots state, read data, move the robot, create and play a song, and read the light
+bump sensors. 
+start - starts the OI and set the
+ robot to passive mode
 passive - set the robot to passive mode
-reset - hard reset of the robot as if you have removed the battery from the robot
-stop - close the OI 
+reset - hard reset
+ of the robot as if you have removed the battery from the robot
+stop - close the 
+OI 
 safe - set the robot to safe mode
-drive(a,b,c,d) - makes the robot drive using a&b as the high and low bits for the velocity 	of the left wheel and c&d for the high and low bits of the velocity of the right 	wheel
-sleep - helper function called to make the program sleep for 0.015 seconds to prevent 		errors in writing data 
-readButton - returns an integer corresponding to what button is being pressed on the robot
+drive(a,b,c,d) - makes the robot drive using 
+a&b as the high and low bits for the velocity 	of the left wheel and c&d for the 
+high and low bits of the velocity of the right 	wheel
+sleep - helper function called
+ to make the program sleep for 0.015 seconds to prevent errors in writing data 
+readButton 
+- returns an integer corresponding to what button is being pressed on the robot. 
 
-project1.py is the driver class which contains the main function of the program. This is the file intended to be called upon and used by the user. This driver class opens the serial connection and initializes the robot to both passive and safe mode. It then waits for the initial clean button press to start the robots hexagonal movement. If at any point the clean button is pressed the robot will stop at the next vertices and wait until the clean button is once again pressed. This process will continue until the program termination indicated by the completion of the hexagon. 
+
+project4.py is the driver class which contains
+ the main function of the program. This is the file intended to be called upon 
+and used by the user. This driver class opens the serial connection and initializes
+ the robot to both passive and safe mode. It then waits for the initial clean 
+button press to start the robots wall following functionality. If at any point the 
+clean button is pressed the robot will stop. If the robot finds a dock, it will change
+its objectives and track and drive to the dock. After it completes these actions, it will
+stop and play a song, and then the program will terminate. The wall following implements 
+a PD controller that keeps the robot following the wall around a 'setpoint' value. 
+The gains can be adjusted as constants at the top of project4.py
--- interface.py
+++ interface.py
@@ -19,8 +19,12 @@ def stop():
 def safe():
     connection.write(131)
 
+# set the robot to full mode
+def full():
+    connection.write(132)
+
 # uses opcode 145 (driveDirect) to drive the robot.
-def drive(a, b, c, d):
+def driveDirect(a, b, c, d):
     connection.write(145)
     connection.write(a)
     connection.write(b)
@@ -52,4 +56,239 @@ def readButton():
     # unpacks the data read to byte.
     byte = struct.unpack('B' ,data)[0]
     return byte 
-    
+# bump and wheel drop sensor read
+def bumpWheelDrop(index):
+    # requests sensor packet data
+    connection.write(142)
+    # wait for command to be sent
+    sleep()
+    # request the bump sensor packet
+    connection.write(7)
+    sleep()
+    data = connection.read(1)
+    # interbret data as byte
+    byte = struct.unpack('B', data)[0]
+    # right bump sensor
+    bumpRight = bool(byte & 0x01)
+    bumpLeft = bool(byte & 0x02)
+    wheelDropRight = bool(byte & 0x04)
+    wheelDropLeft = bool(byte & 0x08)
+    # return bumpRight
+    if (index == 0):
+        return bumpRight
+    elif (index == 1):
+        return bumpLeft
+    elif (index == 2):
+        return wheelDropRight
+    elif (index == 3):
+        return wheelDropLeft
+    elif (index == 4):
+        return bool(byte != 0)
+    else:
+        return byte
+# cliff sensor reading takes param idicating which sensor
+def cliffRead():
+    # declare array to hold results from cliffArray
+    cliffArr = [0,0,0,0,0]
+    counter = 0
+    for i in range(9,14):
+        # requests sensor data
+        connection.write(142)
+        #requests each cliff sensor
+        connection.write(i)
+        data = connection.read(1)
+        byte = struct.unpack('B', data)[0]
+        cliffArr[counter] = byte
+        counter += 1
+    return cliffArr
+# angle
+def angleRead():
+    connection.write(142)
+    # packet 20 is angle 
+    sleep()
+    connection.write(20)
+    # returns signed 2 bytes
+    sleep()
+    data = connection.read(2)
+    byte = struct.unpack(">h", data)[0]
+    return byte
+
+def distanceRead():
+    connection.write(142)
+    sleep()
+    connection.write(19)
+    sleep()
+    data = connection.read(2)
+    byte = struct.unpack(">h", data)[0]
+    return byte
+
+def song():
+    connection.write(140)
+    sleep()
+    connection.write(0)
+    sleep()
+    # first 16 notes of "Seek and Destroy"
+    connection.write(16)
+    sleep()
+    # A(110) * 2
+    connection.write(45)
+    sleep()
+    connection.write(14)
+    sleep()
+    connection.write(45)
+    sleep()
+    connection.write(14)
+    sleep()
+    # triplet D(294) D#(311) D(294) @ 9
+    connection.write(62)
+    sleep()
+    connection.write(9)
+    sleep()
+    connection.write(63)
+    sleep()
+    connection.write(9)
+    sleep()
+    connection.write(62)
+    sleep()
+    # may be 9 || 14
+    connection.write(14)
+    sleep()
+    # A(110)
+    connection.write(45)
+    sleep()
+    connection.write(14)
+    sleep()
+    #triplet A(220) A#(233) A(220) @ 9
+    connection.write(57)
+    sleep()
+    connection.write(9)
+    sleep()
+    connection.write(58)
+    sleep()
+    connection.write(9)
+    sleep()
+    connection.write(57)
+    sleep()
+    # may be 14  || 9
+    connection.write(14)
+    sleep()
+    # A(110) F(175) @ 14
+    connection.write(45)
+    sleep()
+    connection.write(14)
+    sleep()
+    connection.write(53)
+    sleep()
+    connection.write(14)
+    sleep()
+    # E(175) G(196)
+    connection.write(52)
+    sleep()
+    connection.write(14)
+    sleep()
+    connection.write(55)
+    sleep()
+    connection.write(14)
+    sleep()
+    # E(165) C(262)
+    connection.write(52)
+    sleep()
+    connection.write(14)
+    sleep()
+    connection.write(60)
+    sleep()
+    connection.write(14)
+    sleep()
+    # A(220)
+    connection.write(57)
+    sleep()
+    connection.write(14)
+    sleep()
+def play():
+    connection.write(141)
+    connection.write(0)
+# Light Bump sensor range 0-4095
+
+# Light Bump Left
+def LBLeft():
+    connection.write(142)
+    sleep()
+    connection.write(46)
+    sleep()
+    # returns 2 unsigned bytes
+    data = connection.read(2)
+    byte = struct.unpack(">H", data)
+    return int(byte[0])
+# Light Bump Front Left
+def LBFrontLeft():
+    connection.write(142)
+    sleep()
+    connection.write(47)
+    sleep()
+    # returns 2 unsigned bytes
+    data = connection.read(2)
+    byte = struct.unpack(">H", data)
+    return int(byte[0])
+# Light Bump Center Left
+def LBCenterLeft():
+    connection.write(142)
+    sleep()
+    connection.write(48)
+    sleep()
+    # returns 2 unsigned bytes
+    data = connection.read(2)
+    byte = struct.unpack(">H", data)
+    return int(byte[0])
+# Light Bump Center Right
+def LBCenterRight():
+    connection.write(142)
+    sleep()
+    connection.write(49)
+    sleep()
+    # returns 2 unsigned bytes
+    data = connection.read(2)
+    byte = struct.unpack(">H", data)
+    return int(byte[0])
+# Light Front Right
+def LBFrontRight():
+    connection.write(142)
+    sleep()
+    connection.write(50)
+    sleep()
+    # returns 2 unsigned bytes
+    data = connection.read(2)
+    byte = struct.unpack(">H", data)
+    return int(byte[0])
+# Light Bump Right
+def LBRight():
+    connection.write(142)
+    sleep()
+    connection.write(51)
+    sleep()
+    # returns 2 unsigned bytes
+    data = connection.read(2)
+    byte = struct.unpack(">H", data)
+    return int(byte[0])
+# Infrared Character Omni
+def IRCharOmni():
+    connection.write(142)
+    sleep()
+    connection.write(17)
+    sleep()
+    # reads data from the buttons.
+    data = connection.read(1)
+    # unpacks the data read to byte.
+    byte = struct.unpack('B' ,data)[0]
+    return int(byte)
+# Charing source available (2 ||3)
+def charge():
+    connection.write(142)
+    sleep()
+    connection.write(34)
+    sleep()
+    # reads data from the buttons.
+    data = connection.read(1)
+    # unpacks the data read to byte.
+    byte = struct.unpack('B' ,data)[0]
+    return byte
+
